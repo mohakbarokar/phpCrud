d6056142eaf5f3f8e437e514589855c8a4b3f8a0
Mohak Barokar <mohak.barokar@ril.com>
2023-08-30 16:56:11 +0530

    changed file with pg_connect

--- dbcon.php
+++ dbcon.php
@@ -6,7 +6,10 @@ define("USERNAME", "azureuser");
 define("PASSWORD", "Db@Password1234");
 define("DATABASE", "postgres");
 
-$connection = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+//$connection = pg_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+
+$connection = pg_connect("host=jhhdevopsterra-pgsql-server.postgres.database.azure.com dbname=postgres user=azureuser password=Db@Password1234");
+
 
 if (!$connection) {
     die("Connection Failed");
