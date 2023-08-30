fad1f3fd052c50d9e1d4ae5c8705cfa5576aba47
Mohak Barokar <mohak.barokar@ril.com>
2023-08-30 18:19:07 +0530

    Added create file

--- dbconnew.php
+++ dbconnew.php
@@ -1,19 +1,33 @@
 <?php
-$dbconn = pg_connect("host=jhhdevopsterra-pgsql-server.postgres.database.azure.com port=5432 dbname=postgres user=azureuser password=Db@Password1234");
-//connect to a database named "postgres" on the host "host" with a username and password
-if (!$dbconn){
-    echo "<center><h1>Doesn't work =(</h1></center>";
-}else
-    echo "<center><h1>Good connection</h1></center>";
-pg_close($dbconn);
+//$dbconn = pg_connect("host=jhhdevopsterra-pgsql-server.postgres.database.azure.com port=5432 dbname=postgres user=azureuser password=Db@Password1234");
+////connect to a database named "postgres" on the host "host" with a username and password
+//if (!$dbconn){
+//    echo "<center><h1>Doesn't work =(</h1></center>";
+//}else
+//    echo "<center><h1>Good connection</h1></center>";
+//pg_close($dbconn);
 
+$host = 'jhhdevopsterra-pgsql-server.postgres.database.azure.com'; // Change this if your database is hosted elsewhere
+$dbname = 'postgres';
+$user = 'azureuser';
+$password = 'Db@Password1234';
+
+try {
+    $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (!$db) {
+        echo "Connected";
+    }
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $email = $_POST['email'];
 
     // Prepare the SQL query
-    $stmt = $dbconn->prepare("INSERT INTO users (name, email) VALUES (:name, :email)");
+    $stmt = $db->prepare("INSERT INTO users (name, email) VALUES (:name, :email)");
 
     // Bind parameters
     $stmt->bindParam(':name', $name);
