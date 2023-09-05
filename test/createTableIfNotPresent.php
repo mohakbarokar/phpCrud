5d3542dd3e05e1db84b302812b713f6096805c6f
Mohak Barokar <mohak.barokar@ril.com>
2023-09-05 11:15:24 +0530

    Added createTableIfNotPresent.php file in test

--- test/createTableIfNotPresent.php
+++ test/createTableIfNotPresent.php
@@ -14,7 +14,7 @@ if (!$conn) {
 }
 
 // Check if the table already exists
-$tableName = '';
+$tableName = 'book';
 $tableExistsQuery = "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '$tableName')";
 $tableExistsResult = pg_query($conn, $tableExistsQuery);
 
